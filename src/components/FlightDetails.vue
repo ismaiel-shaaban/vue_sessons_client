e421d5e07aac94f2c9e33ff7dac88e9cd6038aa4
ismaiel-shaaban <som3a1941998@gmail.com>
2023-12-07 21:22:40 +0200

    drag and drop

--- src/components/FlightDetails.vue
+++ src/components/FlightDetails.vue
@@ -526,7 +526,8 @@ const submission = async () => {
             if (flightDetails.value.allowReturn != 1 && flightDetails.value.numTickets >= bookingInfo.value.adults_count) {
                 const tickets = new FormData()
                 tickets.append("ticket_id", flightDetails.value.id)
-                tickets.append("new_number_of_tickets", flightDetails.value.numTickets - bookingInfo.value.adults_count)
+                tickets.append("new_number_of_tickets", 1)
+                console.log('mmmmmm' ,tickets);
                 axios.post("https://seasonreal.seasonsge.com/appv1real/ticket-out", tickets)
                     .then(data => {
                         debugger
@@ -552,7 +553,8 @@ const submission = async () => {
             } else if (flightDetails.value.allowReturn == 1 && flightDetails.value.numReturnTickets >= bookingInfo.value.adults_count && flightDetails.value.numTickets >= bookingInfo.value.adults_count) {
                 const tickets = new FormData()
                 tickets.append("ticket_id", flightDetails.value.id)
-                tickets.append("new_number_of_tickets", flightDetails.value.numTickets - bookingInfo.value.adults_count)
+                tickets.append("new_number_of_tickets", 1)
+                console.log('nnnnnnnnn' ,tickets);
                 axios.post("https://seasonreal.seasonsge.com/appv1real/ticket-out", tickets)
                     .then(data => {
                         console.log(data);
