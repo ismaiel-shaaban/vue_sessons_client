03b36bbdb74d3ee58a9a99786faf168324eac99a
ismaiel-shaaban <som3a1941998@gmail.com>
2023-12-14 12:05:59 +0200

    drag and drop

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   // base: '/seasons-tour/',
   plugins: [vue()],
+  build: {
+    // Set the chunk size warning limit in kilobytes
+    chunkSizeWarningLimit: 1000,
+
+    // Configure manual chunks to group specific dependencies together
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Grouping lodash into a separate chunk
+          lodash: ['lodash'],
+        },
+      },
+    },}
 })
